4f34b44f694b45747babdd469ee2d4984350b2fb
Riklef <riklef.irps@gmail.com>
2021-02-09 01:03:40 +0100

    added car model

---
+++ source/src/CarRent/Models/Car.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRent.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Manufacturer { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Model { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Class { get; set; }
+    }
+}
