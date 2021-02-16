20bf67c42f475c13e05af06f3d7931864189bd7b
Riklef <riklef.irps@gmail.com>
2021-02-16 18:14:43 +0100

    minor changes

--- source/src/CarRent/Controllers/CustomersController.cs
+++ source/src/CarRent/Controllers/CustomersController.cs
@@ -51,13 +51,5 @@ namespace CarRent.Controllers
             
             return Ok(_mapper.Map<IEnumerable<CustomerReadDto>>(customerItems));
         }
-
-        /*
-        //GET api/ControllerName?p1=1&p2=2&p3=3&p4=4&p5=5
-        [HttpGet]
-        [Route("")]
-        public IHttpActionResult GetItemByNameAndId(string p1, string p2, string p3, string p4, string p5) {​​
-            //...return Ok(someResult);
-        }​​*/
     }
 }
