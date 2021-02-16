fb192f2575cc78cb6d86447f79d45cdf991d5534
Riklef <riklef.irps@gmail.com>
2021-02-16 23:51:40 +0100

    added edited customer to migrations

---
+++ source/src/CarRent/Migrations/20210216224810_CustomerMigration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRent.Migrations
+{
+    public partial class CustomerMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Customers",
+                type: "varchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Postalcode",
+                table: "Customers",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Customers",
+                type: "varchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Postalcode",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Customers");
+        }
+    }
+}
