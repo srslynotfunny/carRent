7aa07d9fbcae4ce5fc9c3fbf0a158a7d32e7f367
Riklef <riklef.irps@gmail.com>
2021-02-16 23:44:23 +0100

    added properties to Customer Model

--- source/src/CarRent/Models/Customer.cs
+++ source/src/CarRent/Models/Customer.cs
@@ -14,5 +14,17 @@ namespace CarRent.Models
         [Required]
         [MaxLength(250)]
         public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Street { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string City { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Postalcode { get; set; }
     }
 }
